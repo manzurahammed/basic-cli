63ab9c2b1a8a0b83a188d6a9d381b12c292f4e5f
Manzur Ahammed <manzur.ahammed236@gmail.com>
2021-02-16 01:33:10 +0600

    swithc method

--- client/switch.go
+++ client/switch.go
@@ -12,5 +12,67 @@ type Switch struct {
 
 func NewSwitch(uri string){
 	httpClient := NewHTTPClient(uri);
-	return Switch{}
+	s := Switch{
+		client:httpClient,
+		backendurl:uri,
+	}
+	s.command = map[string] func() func(string) error {
+		"create":s.create,
+		"edit":s.edit,
+		"delete":s.delete,
+		"fetch":s.fetch,
+		"health":s.health,
+	}
+	return s
+}
+
+func (s Switch) Switch() error{
+	cmdName := os.Args[1]
+	cmd, ok := s.command[cmdName]
+	if !ok {
+		fmt.ErrorF("Invalid command %s",cmdName)
+	}
+	return cmd()(cmdName)
+}
+
+func (s Switch) create() func(string) error{
+	return func(cmd strinf) error{
+		fmt.Println("Create Reminder")
+		return nil
+	}
+}
+
+func (s Switch) create() func(string) error{
+	return func(cmd strinf) error{
+		fmt.Println("Create Reminder")
+		return nil
+	}
+}
+
+func (s Switch) edit() func(string) error{
+	return func(cmd strinf) error{
+		fmt.Println("Create Reminder")
+		return nil
+	}
+}
+
+func (s Switch) delete() func(string) error{
+	return func(cmd strinf) error{
+		fmt.Println("Create Reminder")
+		return nil
+	}
+}
+
+func (s Switch) fetch() func(string) error{
+	return func(cmd strinf) error{
+		fmt.Println("Create Reminder")
+		return nil
+	}
+}
+
+func (s Switch) health() func(string) error{
+	return func(cmd strinf) error{
+		fmt.Println("Create Reminder")
+		return nil
+	}
 }
